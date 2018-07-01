e987563a37daa5c63b49c73c72dfa196c3f2caf1
gyarados025 <40200770+gyarados025@users.noreply.github.com>
2018-07-01 20:45:02 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -108,6 +108,17 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := LauncherConfig
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/app/LauncherConfig/LauncherConfig.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := atfwd
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/atfwd/atfwd.apk
